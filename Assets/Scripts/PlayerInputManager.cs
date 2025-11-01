723d5244155e3e1f44607fd71f5faeb3907c840f
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2025-11-01 22:43:28 +0000

    Improve controller fallback behavior

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -12,14 +12,21 @@ public class PlayerInputManager : MonoBehaviour
         var pads = Gamepad.all;
         Debug.Log(pads.Count);
 
-        if (pads.Count < 2)
+        if (pads.Count == 0)
         {
-            Debug.LogError("Did not find 2 gamepads, falling back to keyboard");
+            Debug.LogError("Did not find, falling back to keyboard");
             _player1.SwitchCurrentControlScheme("WASD", Keyboard.current);
             _player2.SwitchCurrentControlScheme("Arrows", Keyboard.current);
         }
-
-        _player1.SwitchCurrentControlScheme("Gamepad", pads[0]);
-        _player2.SwitchCurrentControlScheme("Gamepad", pads[1]);
+        else if (pads.Count == 1)
+        {
+            Debug.LogError("Only one controller found, P1 uses controller, P2 can move with WASD");
+            _player1.SwitchCurrentControlScheme("Gamepad", pads[0]);
+            _player2.SwitchCurrentControlScheme("WASD", Keyboard.current);
+        }else
+        {
+            _player1.SwitchCurrentControlScheme("Gamepad", pads[0]);
+            _player2.SwitchCurrentControlScheme("Gamepad", pads[1]);
+        }       
     }
 }
